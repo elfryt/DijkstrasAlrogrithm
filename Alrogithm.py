a513b942e626bf358a579a2adfeab475a350e160
ELFR <61186893+elfryt@users.noreply.github.com>
2022-02-07 17:55:27 +0000

    Fixed bugs from report #1
    Works perfectly. Copy line 8-14 and change values to make more branches.
    
    Done on iPad

--- Alrogithm.py
+++ Alrogithm.py
@@ -1,18 +1,17 @@
 a = 1
-a.child1 = 3
-a.child2 = 4
+achild1 = 3
+achild2 = 1
 b = 3
-c = 4									#change values here
-S = 0
-E = 0
+c = 4					#change values here
+path = 0
+	
+def compareAchild():
+	if achild1 < achild2:
+		path = "a, a.child1"	#compare achild1 and achild2
+		print(path)		#shows shortest path
+	elif achild2 < achild1:
+		path = "a, a.child2"
+		print(path)
 
 if a < b:
-	compareA.child()
-	
-compareA.child():
-	if a.child1 < a.child2:
-		E = 'a, a.child1'
-		console.log(E)
-	elseif a.child2 < a.child1:
-		E = 'a, a.child2'
-		console.log(E)
+	compareAchild()			#calls to compare
