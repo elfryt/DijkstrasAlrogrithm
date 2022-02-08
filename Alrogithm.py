6a59a729c8b63bdd4cbb9d4d87bf19af42c34d61
ELFR <61186893+elfryt@users.noreply.github.com>
2022-02-08 12:59:35 +0000

    Update Alrgorithm.py
    This update looks into the selected shortest route's children, checking if the child's route is larger than the other child's.
    Output should now be null, working on fixing it.

--- Alrogithm.py
+++ Alrogithm.py
@@ -2,6 +2,8 @@ a = 1
 achild1 = 3
 achild2 = 1
 b = 3
+bchild1 = 4
+bchild2 = 2
 c = 4					#change values here
 path = 0
 	
@@ -12,6 +14,17 @@ def compareAchild():
 	elif achild2 < achild1:
 		path = "a, a.child2"
 		print(path)
+		
+def compareBchild():
+	if bchild1 < bchild2:
+		path = "b, bchild1"
+		print(path)
+	elif bchild2 < bchild1:
+		path = "b, bchild2"
+		print(path)
 
-if a < b:
+if a < b and achild1 + achild2 < bchild1 + bchild2:
 	compareAchild()			#calls to compare
+	
+elif b < a and bchild1 + bchild2 < achild1 + achild2:
+	compareBchild()
